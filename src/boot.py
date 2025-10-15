5d1e13ccdd7fe53ffeba2eabf77ef3e71db024c5
vilRenard <ismaelchouli@hotmail.fr>
2025-10-15 15:42:42 +0200

    update boot

--- src/boot.py
+++ src/boot.py
@@ -29,19 +29,31 @@ m.label = "PicoUSB"
 storage.remount("/", readonly=True)
 storage.enable_usb_drive()
 
+
 time.sleep(0.1) #wait a bit so the button gets pulled up
 
 if mode.value:
-    storage.disable_usb_drive()
+    if microcontroller.nvm[0] == 1:
+        microcontroller.nvm[0] = 0
+        storage.enable_usb_drive()
+        microcontroller.on_next_reset(microcontroller.RunMode.SAFE_MODE)
+        microcontroller.reset()
+    else:
+        storage.disable_usb_drive()
 else:
     time.sleep(0.1) #check again after 100ms to see if the button is still pressed
     if mode.value:
-        storage.disable_usb_drive()
+        if microcontroller.nvm[0] == 1:
+            microcontroller.nvm[0] = 0
+            storage.enable_usb_drive()
+            microcontroller.on_next_reset(microcontroller.RunMode.SAFE_MODE)
+            microcontroller.reset()
+        else:
+            storage.disable_usb_drive()
     else:
         storage.enable_usb_drive()
         microcontroller.on_next_reset(microcontroller.RunMode.SAFE_MODE)
         microcontroller.reset()
-    
 
 # in case you screw up and disable usb drive without the ability to enable it, to enter safe mode write in shell:
 # import microcontroller
